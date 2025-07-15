c602d5601bab533e2dff739f30f63ef2887e2d8e
Diego Keller <diego.keller@campspot.com>
2025-07-15 18:31:17 -0300

    adicionar comentário

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Post, PostService, Comment } from '../../services/post.service';
 import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-post',
-  imports: [NgFor],
+  imports: [NgFor, FormsModule],
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
@@ -13,6 +14,8 @@ export class PostComponent implements OnInit {
 
   post?: Post;
   comments: Comment[] = [];
+
+  newComment: Comment = {} as Comment;
 
   constructor(
     private route: ActivatedRoute,
@@ -33,4 +36,9 @@ export class PostComponent implements OnInit {
     });
   }
 
+  addComment(): void {
+    console.log('Adding comment...', this.newComment);
+    this.comments.unshift(this.newComment);
+    this.newComment = {} as Comment;
+  }
 }
